576440a4d8bb839a386c541bdf3293ed2ac8428e
Daniel <danielgofu8@gmail.com>
2025-12-10 22:56:31 +0100

    Fix project structure and file organization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 export default {
   content: [
     "./index.html",
-    "./index.tsx",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
@@ -33,5 +32,4 @@ export default {
     }
   },
   plugins: [],
-
 }
