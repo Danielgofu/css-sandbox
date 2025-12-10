9da94d36c7e3acb4b2388452d9ed38a4bc011d4f
Daniel <danielgofu8@gmail.com>
2025-12-10 23:26:28 +0100

    Github pages error fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/css-sandbox/',
+  base: "/css-sandbox/",
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'esbuild',
     rollupOptions: {
       output: {
-        entryFileNames: 'index.js',
-        chunkFileNames: 'chunk-[hash].js',
-        assetFileNames: '[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
       },
     },
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
   },
   resolve: {
     alias: {
